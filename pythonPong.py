c0c6db3825b95fcf19d57fe178186591beb616e5
JPMorris510 <joshpowellmorris@gmail.com>
2024-07-02 14:02:15 +0100

    Initial commit

---
+++ pythonPong.py
@@ -0,0 +1,34 @@
+import turtle
+
+# Initialise variables
+'''Your code here'''
+
+# Create the screen
+'''Your code here'''
+
+# Create Left Paddle
+'''Your code here'''
+
+# Create Right Paddle
+'''Your code here'''
+
+# Create ball
+'''Your code here'''
+
+# Create the fuctions which control paddle movement
+'''Your code here'''
+
+# Create listeners for the inputs using turtle
+'''Your code here'''
+
+# Game Logic
+'''Create a while loop and tell the screen to update'''
+
+# Detect collision with top and bottom and make the ball bounce off
+'''Your code here'''
+
+# Detect collision with the left and right edge, award a point to the other player and reset the ball
+'''Your code here'''
+
+# Detect collision with the paddles and make the ball bounce away
+'''Your code here'''
